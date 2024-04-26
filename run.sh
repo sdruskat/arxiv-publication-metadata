19dc67f02663901a2c83aa6850f93600fd0da72e
Stephan Druskat <stephan.druskat@dlr.de>
2024-04-26 17:16:00 +0200

    Add SDM to run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Running Snakemake workflow with 12 cores, keeping local storage."
-snakemake --keep-storage-local-copies -c 12
+snakemake --keep-storage-local-copies --software-deployment-method conda -c 12
 echo "Done."
 

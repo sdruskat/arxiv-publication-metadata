71c0d16e8534180ad851dc0028c2f7bf573b9d5f
Stephan Druskat <stephan.druskat@dlr.de>
2024-04-29 11:49:07 +0200

    Add script for splitting LUTs

---
+++ scripts/split_luts.py
@@ -0,0 +1,38 @@
+import json
+import logging
+
+log = logging.getLogger(__name__)
+
+file_handler = logging.FileHandler(f"{snakemake.output[0]}.log", mode="w")
+file_handler.setFormatter(logging.Formatter(
+    fmt="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(module)s.%(funcName)s > %(filename)s:%(lineno)s)"
+))
+log.setLevel(logging.getLevelName("DEBUG"))
+log.addHandler(file_handler)
+
+
+patched_lut = snakemake.input[0]
+out_dir = snakemake.output[0]
+
+luts = dict()
+
+with open(patched_lut, "r") as lutf:
+    data = json.load(lutf)
+
+    data_len = len(data)
+
+    for i, (k, v) in enumerate(data.items()):
+        if i % 100000 == 0:
+            log.debug(f"Splitting item {i} of {data_len} ({i / data_len * 100}%)")
+        file_name = k.split("/")[0].split(".")[0] + ".json"
+        if file_name in luts:
+            luts[file_name] = luts[file_name] | {k: v}
+        else:
+            luts[file_name] = {k: v}
+
+luts_count = len(luts)
+
+for i, (file, lut) in enumerate(luts.items()):
+    log.debug(f"Writing LUT {i + 1} ({file}) of {luts_count}")
+    with open(f"{out_dir}{file}", "w") as out:
+        json.dump(lut, out)
